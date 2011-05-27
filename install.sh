a14421a1bd02d6b8a272a236f858156c21405324
fredgnix <fredgnix@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2011-05-27 09:43:23 +0000

    INSTALL & ChangeLog updated

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+PERLVER=5.12.3
+PERLDIR=usr/lib${LIBDIRSUFFIX}/perl5/$PERLVER
+
 install -D -m 755 twapake $DESTDIR/usr/sbin/twapake
+install -D -m 644 Twapake.pm $DESTDIR/$PERLDIR/Twapake/Twapake.pm
+install -D -m 644 Twacurse.pm $DESTDIR/$PERLDIR/Twapake/Twacurse.pm
 install -D -m 755 twapake.cron $DESTDIR/etc/cron.daily/twapake
-#install -D -m 644 man/twapake.man $DESTDIR/usr/man/man8/twapake.1
+install -D -m 644 twapake.man $DESTDIR/usr/man/man8/twapake.8
